cc080c59a87e452b3b63e6745e339169dfc97b7b
Schnoog <schnoog@schnoog.eu>
2026-09-11 14:33:47 +0200

    next save

--- compile_and_upload.sh
+++ compile_and_upload.sh
@@ -4,4 +4,4 @@ cd $(dirname $(realpath $0))
 
 which esphome || source ../venv/bin/activate
 
-esphome compile big.yaml && esphome upload big.yaml --device 192.168.178.108
+esphome compile big.yaml && esphome upload big.yaml --device 192.168.178.108 && esphome logs big.yaml --device 192.168.178.108
